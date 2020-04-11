d138b30e37769e4122a758452afc7c333d9e2abc
xaviermsd7 <harshprajapati34@gmail.com>
2020-04-11 18:45:03 +0530

    "Updated Folder with Phase -2 | Project -1"

---
+++ Basics/Phase - 2/Project 1/js/app.js
@@ -0,0 +1,77 @@
+// console.log('This is Notes add Project and working in app.js')
+showNotes()
+//If user adds a note, add it to a localstorage
+let addBtn = document.getElementById('addNote')
+addBtn.addEventListener('click', function (_e) {
+  let addDetails = document.getElementById('addDetails')
+  //let addTitle = document.getElementById('addTitle')
+  let notes = localStorage.getItem('notes')
+  if (notes == null) {
+    noteObj = []
+  } else {
+    noteObj = JSON.parse(notes)
+  }
+  noteObj.push(addDetails.value)
+  //   noteObj.push(addTitle.value)
+  localStorage.setItem('notes', JSON.stringify(noteObj))
+  addDetails.value = ''
+  //addTitle.value = ''
+  //   console.log(noteObj)
+  showNotes()
+})
+//Function to Show the Note
+function showNotes () {
+  let notes = localStorage.getItem('notes')
+  if (notes == null) {
+    noteObj = []
+  } else {
+    noteObj = JSON.parse(notes)
+  }
+  let html = ''
+  noteObj.forEach(function (element, index) {
+    html += `<div class="noteCard card mx-2 my-2" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">Note ${index + 1}</h5>
+                            <p class="card-text">${element}</p>
+                            <button id="${index}" onclick= "deletNotes(this.id)" class="btn btn-primary">Delete</a>
+                        </div>
+                    </div>`
+  })
+  let notesElm = document.getElementById('notesDisplay')
+  if (noteObj.length != 0) {
+    notesElm.innerHTML = html
+  } else {
+    notesElm.innerHTML = `Noting to how her - Please add note above`
+  }
+}
+
+//Function to Delete the Note
+function deletNotes (index) {
+  //   console.log('Im Deleting This Note :' + index)
+
+  let notes = localStorage.getItem('notes')
+  if (notes == null) {
+    noteObj = []
+  } else {
+    noteObj = JSON.parse(notes)
+  }
+  noteObj.splice(index, 1)
+  localStorage.setItem('notes', JSON.stringify(noteObj))
+  showNotes()
+}
+
+//Search Filter
+let search = document.getElementById('searchText')
+search.addEventListener('input', function () {
+  let inputVal = search.value.toLowerCase()
+  //   console.log('input event fired!' + inputVal)
+  let noteCards = document.getElementsByClassName('noteCard')
+  Array.from(noteCards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName('p')[0].innerText
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = 'block'
+    } else {
+      element.style.display = 'none'
+    }
+  })
+})
---
+++ Basics/Phase - 2/js/main.js
@@ -0,0 +1,21 @@
+console.log('ok')
+let myArray = [1, 2, 3, 4, 5]
+myArray.forEach(element => {
+  console.log(element)
+})
+myArray.forEach(ok => {
+  console.log(ok)
+})
+let myObj = {
+    firstName:'ok',
+    lastName:'ok2',
+    age:23,
+    emailId:'harshprajapati34@gmail.com',
+};
+console.log(myObj);
+for (const key in myObj) {
+    if (myObj.hasOwnProperty(key)) {
+        const element2= myObj[key];
+        console.log(element2);
+    }
+}
